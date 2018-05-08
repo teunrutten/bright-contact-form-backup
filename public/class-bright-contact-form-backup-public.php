2b550c5aacbfd10d2c79a9b4ccef3ecee6333ada
Teun Rutten <teun.creative@gmail.com>
2018-05-08 16:45:44 +0200

    disable enqueues

--- public/class-bright-contact-form-backup-public.php
+++ public/class-bright-contact-form-backup-public.php
@@ -73,7 +73,7 @@ class Bright_Contact_Form_Backup_Public {
 		 * class.
 		 */
 
-		wp_enqueue_style( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'css/bright-contact-form-backup-public.css', array(), $this->version, 'all' );
+		// wp_enqueue_style( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'css/bright-contact-form-backup-public.css', array(), $this->version, 'all' );
 
 	}
 
@@ -96,7 +96,7 @@ class Bright_Contact_Form_Backup_Public {
 		 * class.
 		 */
 
-		wp_enqueue_script( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'js/bright-contact-form-backup-public.js', array( 'jquery' ), $this->version, false );
+		// wp_enqueue_script( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'js/bright-contact-form-backup-public.js', array( 'jquery' ), $this->version, false );
 
 	}
 
