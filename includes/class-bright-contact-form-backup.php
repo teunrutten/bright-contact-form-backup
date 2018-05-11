ec5cda2ed59dd51ab9ed634a531be58c316d87fe
Teun Rutten <teun.creative@gmail.com>
2018-05-11 13:21:05 +0200

    update defined

--- includes/class-bright-contact-form-backup.php
+++ includes/class-bright-contact-form-backup.php
@@ -67,8 +67,8 @@ class Bright_Contact_Form_Backup {
 	 * @since    1.0.0
 	 */
 	public function __construct() {
-		if ( defined( 'PLUGIN_NAME_VERSION' ) ) {
-			$this->version = PLUGIN_NAME_VERSION;
+		if ( defined( 'BRIGHT_CONTACT_FORM_BACKUP_VERSION' ) ) {
+			$this->version = BRIGHT_CONTACT_FORM_BACKUP_VERSION;
 		} else {
 			$this->version = '1.0.0';
 		}
